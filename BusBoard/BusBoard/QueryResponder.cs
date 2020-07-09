42596be5ee80f502171d847def272f6acc69822d
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-09 17:55:24 +0700

    Refactor the QueryResponse

---
+++ BusBoard/BusBoard/QueryResponder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusBoard
+{
+    public class QueryResponder
+    {
+        public static void PrintBusArrivalSchedule(List<IncomingBusPrediction> incomingBusPredictions)
+        {
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("|{0,-10}|{1,-25}|{2,-25}|", "Bus Number", "Final Stop", "Time to Arrival (seconds)");
+            Console.WriteLine("----------------------------------------------------------------");
+            foreach (var incomingBusPrediction in incomingBusPredictions.Take(5))
+            {
+                incomingBusPrediction.Print();
+            }
+        }
+    }
+}
