5bf03ced14e30f808522033c18d057e5ce9f3d0c
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-09 17:35:34 +0700

    Create a program that takes a stop ID and returns the next 5 bus arrivals

---
+++ BusBoard/BusBoard/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace BusBoard
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a bus stop ID: ");
+                var stopId = Console.ReadLine();
+
+                var tflApiUrl = "https://api.tfl.gov.uk";
+
+                var request = new RestRequest($"StopPoint/{stopId}/Arrivals");
+
+                var incomingBusPredictions =
+                    JsonConvert.DeserializeObject<List<IncomingBusPrediction>>(new RestClient(tflApiUrl).Get(request)
+                        .Content);
+
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.WriteLine("|{0,-10}|{1,-25}|{2,-25}|", "Bus Number", "Final Stop", "Time to Arrival (seconds)");
+                Console.WriteLine("----------------------------------------------------------------");
+                foreach (var incomingBusPrediction in incomingBusPredictions.Take(5))
+                {
+                    incomingBusPrediction.Print();
+                }
+            }
+        }
+    }
+}
---
+++ BusBoard/BusBoard/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BusBoard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BusBoard")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17F9A87E-74FB-4363-8EF7-B13339A7991E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
