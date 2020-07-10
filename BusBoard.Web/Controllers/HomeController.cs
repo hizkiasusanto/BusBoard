5622d7e0f2a1c6a8b219bdb163e4851f214a8f37
Hizkia Susanto <hizkia.susanto@st-hughs.ox.ac.uk>
2020-07-10 18:23:35 +0700

    Output bus schedule as a table, enforce pattern on post code and search radius input, refresh every 30 seconds

--- BusBoard.Web/Controllers/HomeController.cs
+++ BusBoard.Web/Controllers/HomeController.cs
@@ -15,17 +15,24 @@ namespace BusBoard.Web.Controllers
     [HttpGet]
     public ActionResult BusInfo(PostcodeSelection selection)
     {
-      // Add some properties to the BusInfo view model with the data you want to render on the page.
-      // Write code here to populate the view model with info from the APIs.
-      // Then modify the view (in Views/Home/BusInfo.cshtml) to render upcoming buses.
+      HttpContext.Response.Headers.Add("refresh", "30; url=" + Url.Action("BusInfo",selection));
+      
       var info = new BusInfo(selection.Postcode, selection.SearchRadius);
       info.Coordinate = new PostCodeApiHandler().GetCoordinate(selection.Postcode);
       info.NearbyBusStops = new TflApiHandler().GetNearbyBusStops(info.Coordinate,selection.SearchRadius);
 
-      foreach (var nearbyBusStop in info.NearbyBusStops)
+      if (info.NearbyBusStops != null)
       {
-        info.StopWithArrivals.Add(new StopWithArrivals(nearbyBusStop));
+        foreach (var nearbyBusStop in info.NearbyBusStops)
+        {
+          info.StopWithArrivals.Add(new StopWithArrivals(nearbyBusStop));
+        }
       }
+      else
+      {
+        info.StopWithArrivals = null;
+      }
+
       return View(info);
     }
 
